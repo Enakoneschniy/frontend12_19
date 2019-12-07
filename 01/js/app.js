e8dc3b37d4f87d79443b0eb95b0f5408b068d241
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2019-12-07 16:11:03 +0200

    lesson 01

---
+++ 01/js/app.js
@@ -0,0 +1,49 @@
+"use strict";
+console.log('Hello world!');
+var a;
+console.log(a);
+var a = 55;
+// console.log(x);
+const x = 19;
+let y = 19;
+console.log(y); // 19
+{
+    let y = 10;
+    console.log(y); // 10
+}
+console.log(y); // 19
+let num = 12; // Number
+let num1 = 12.2; // Number
+let str = 'string' + num; // String
+let str1 = "string" + num; // String
+let str2 = `string ${num}`; // String
+let str3 = `string 
+${num}
+test
+`; // String
+console.log(str3);
+
+/*
+ блочный
+ комментарий
+*/
+
+const word = 'word';
+const slovo = 'word';
+const слово = 'word';
+
+const userName = 'Vasya';
+let nameFieldChecked = true; // Boolean
+let ageFieldChecked = false; // Boolean
+
+let testNull = null; // null
+let testUndefined = undefined; // undefined
+let testUndefined1; // undefined
+console.log(typeof userName);
+console.log(typeof testNull);
+// testNull = 456;
+
+
+
+
+
